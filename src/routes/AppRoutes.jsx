8c974a18700d21420a3e5d258f79d708f28dd73f
achibhossengit <mail.achibhossen@gmail.com>
2025-05-18 08:00:18 +0600

    remove unneccesay components

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router';
 import MainLayout from '../pages/layouts/MainLayout';
 import About from '../pages/About';
-import Product from '../pages/Product';
 import Home from '../pages/Home';
 import Shop from '../pages/Shop';
+import Login from '../pages/Login';
 
 const AppRoutes = () => {
     return (
@@ -12,8 +12,8 @@ const AppRoutes = () => {
                 <Route element={<MainLayout/>}>
                     <Route path='/' element={<Home/>}/>
                     <Route path='about' element={<About/>}/>
-                    <Route path='product' element={<Product/>}/>
                     <Route path='shop' element={<Shop/>}/>
+                    <Route path='login' element={<Login/>}/>
                 </Route>
             </Routes>   
         </>
