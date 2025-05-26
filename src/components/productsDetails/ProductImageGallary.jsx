c932bca4a8dcf47d26288c5bc8d2117b6ab15964
achibhossengit <mail.achibhossen@gmail.com>
2025-05-26 15:22:15 +0600

    fix default image related issue

--- src/components/productsDetails/ProductImageGallary.jsx
+++ src/components/productsDetails/ProductImageGallary.jsx
@@ -7,13 +7,11 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import { Navigation, Pagination, Thumbs } from "swiper/modules";
 import { useState } from "react";
+import defaultImage from "../../assets/images/default_product.jpg"
 
 const ProductImageGallary = ({ images }) => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
-  if (images.length <= 0) {
-    // default image is not working
-    images = [{ image: "../../assets/images/default_product.jpg" }];
-  }
+  const displayImages = images.length > 0 ? images : [{image: defaultImage}]
 
   console.log(images);
   return (
@@ -24,7 +22,7 @@ const ProductImageGallary = ({ images }) => {
         navigation
         pagination={{ clickable: true }}
       >
-        {images.map((imageObj, index) => (
+        {displayImages.map((imageObj, index) => (
           <SwiperSlide>
             <div className="aspect-square bg-base-200">
               <img
