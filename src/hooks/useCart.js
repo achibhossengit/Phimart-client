3d59804dd147b8e964c6e715815c3733bd2230c0
achibhossengit <mail.achibhossen@gmail.com>
2025-05-27 12:14:49 +0600

    create auth-api-client services using axios interceptor

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,22 +1,14 @@
-import React, { useState } from "react";
-import apiClient from "../services/api-client";
+import { useState } from "react";
+import authApiClient from "../services/auth-api-client";
 
 const useCart = () => {
-  const [authToken, setAuthToken] = useState(
-    () => JSON.parse(localStorage.getItem("authTokens")).access
-  );
   const [cart, setCart] = useState(null);
   const [cartId, setCartId] = useState(() => localStorage.getItem("cartId"));
 
   // create cart
   const createOrGetCart = async () => {
     try {
-      console.log("inside caret try block");
-      const response = await apiClient.post(
-        "/carts/",
-        {},
-        { headers: { Authorization: `JWT ${authToken}` } }
-      );
+      const response = await authApiClient.post("/carts/");
       setCart(response.data);
       setCartId(response.data.id);
       localStorage.setItem("cartId", response.data.id);
@@ -29,22 +21,17 @@ const useCart = () => {
   const addCartItem = async (product_data) => {
     if (!cartId) await createOrGetCart();
     try {
-      const responsse = await apiClient.post(
+      const response = await authApiClient.post(
         `/carts/${cartId}/items/`,
-        product_data,
-        {
-          headers: {
-            Authorization: `JWT ${authToken}`,
-          },
-        }
+        product_data
       );
-      return responsse;
+      return response;
     } catch (error) {
       console.log(error);
     }
   };
 
-  return { createOrGetCart, cart, addCartItem };
+  return { cart, createOrGetCart, addCartItem };
 };
 
 export default useCart;
